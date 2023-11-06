3cb565b41a9ab55333b482c702e3ba983f69f3af
Alex <alexdevogel@gmail.com>
2023-11-06 20:49:27 +0100

    fix

---
+++ .next/server/chunks/345.js
@@ -0,0 +1,292 @@
+exports.id = 345;
+exports.ids = [345];
+exports.modules = {
+
+/***/ 84910:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 31232, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 52987, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 50831, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 56926, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 44282, 23))
+
+/***/ }),
+
+/***/ 74274:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 54293));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 54047))
+
+/***/ }),
+
+/***/ 54047:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(56786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(52451);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(11440);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_navigation__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(57114);
+/* harmony import */ var next_navigation__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_navigation__WEBPACK_IMPORTED_MODULE_3__);
+/* __next_internal_client_entry_do_not_use__ default auto */ 
+
+
+
+const Mobilebar = ()=>{
+    const pathname = (0,next_navigation__WEBPACK_IMPORTED_MODULE_3__.usePathname)();
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("nav", {
+        className: "mobilebar",
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            className: "flex flex-row items-center justify-evenly pt-2",
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                    href: "/legacy",
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                        src: "/icons/tab_nft.png",
+                        height: 30,
+                        width: 30,
+                        className: `mobile-icon mb-[5px] ${pathname == "/legacy" ? "active" : ""}`,
+                        id: "mobtablegacy",
+                        alt: "home"
+                    })
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                    href: "/",
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                        src: "/icons/ark_a.svg",
+                        height: 30,
+                        width: 30,
+                        className: `mobile-icon ${pathname == "/" ? "active" : ""}`,
+                        id: "mobtabhome",
+                        alt: "home"
+                    })
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                    href: "/menu",
+                    children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                        src: "/icons/tab_more.png",
+                        height: 30,
+                        width: 30,
+                        className: `mobile-icon mb-[7px] ${pathname == "/menu" ? "active" : ""}`,
+                        id: "mobtabmenu",
+                        alt: "home"
+                    })
+                })
+            ]
+        })
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Mobilebar);
+
+
+/***/ }),
+
+/***/ 54293:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(56786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(52451);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_navigation__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(57114);
+/* harmony import */ var next_navigation__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_navigation__WEBPACK_IMPORTED_MODULE_2__);
+/* __next_internal_client_entry_do_not_use__ default auto */ 
+
+
+const Sidebar = ()=>{
+    const pathname = (0,next_navigation__WEBPACK_IMPORTED_MODULE_2__.usePathname)();
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("nav", {
+        className: "sidebar",
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+            className: "flex flex-col items-start justify-start",
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                    alt: "ARK Logo",
+                    width: 100,
+                    height: 35,
+                    className: "brand-img",
+                    src: "/images/logo.png"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                    id: "tabhome",
+                    className: `menu-item mt-[75px] mb-3 ${pathname == "/" ? "active" : ""}`,
+                    href: "/",
+                    children: "Home"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                    id: "tabvault",
+                    className: `menu-item my-3 ${pathname == "/vault" ? "active" : ""}`,
+                    href: "/vault",
+                    children: "Vault"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                    id: "tabfoundation",
+                    className: `menu-item my-3 ${pathname == "/foundation" ? "active" : ""}`,
+                    href: "/foundation",
+                    children: "Foundation"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                    id: "tablegacy",
+                    className: `menu-item my-3 ${pathname == "/legacy" ? "active" : ""}`,
+                    href: "/legacy",
+                    children: "Legacy NFT"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                    id: "tabnetwork",
+                    className: `menu-item my-3 ${pathname == "/network" ? "active" : ""}`,
+                    href: "/network",
+                    children: "Network"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                    id: "tabmenu",
+                    className: `menu-item my-3 ${pathname == "/menu" ? "active" : ""}`,
+                    href: "/menu",
+                    children: "Menu"
+                })
+            ]
+        })
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Sidebar);
+
+
+/***/ }),
+
+/***/ 98862:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ RootLayout),
+  metadata: () => (/* binding */ metadata)
+});
+
+// EXTERNAL MODULE: external "next/dist/compiled/react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(56786);
+// EXTERNAL MODULE: ./src/app/globals.css
+var globals = __webpack_require__(5023);
+// EXTERNAL MODULE: ./src/app/utils.css
+var utils = __webpack_require__(56155);
+// EXTERNAL MODULE: ./node_modules/next/dist/build/webpack/loaders/next-flight-loader/module-proxy.js
+var module_proxy = __webpack_require__(61363);
+;// CONCATENATED MODULE: ./src/components/Layout/Sidebar/Sidebar.tsx
+
+const proxy = (0,module_proxy.createProxy)(String.raw`C:\Users\alexd\source\repos\ARK\ARK NEW HOME\src\components\Layout\Sidebar\Sidebar.tsx`)
+
+// Accessing the __esModule property and exporting $$typeof are required here.
+// The __esModule getter forces the proxy target to create the default export
+// and the $$typeof value is for rendering logic to determine if the module
+// is a client boundary.
+const { __esModule, $$typeof } = proxy;
+const __default__ = proxy.default;
+
+
+/* harmony default export */ const Sidebar = (__default__);
+;// CONCATENATED MODULE: ./src/components/Layout/Sidebar/index.ts
+
+
+;// CONCATENATED MODULE: ./src/components/Layout/Mobilebar/Mobilebar.tsx
+
+const Mobilebar_proxy = (0,module_proxy.createProxy)(String.raw`C:\Users\alexd\source\repos\ARK\ARK NEW HOME\src\components\Layout\Mobilebar\Mobilebar.tsx`)
+
+// Accessing the __esModule property and exporting $$typeof are required here.
+// The __esModule getter forces the proxy target to create the default export
+// and the $$typeof value is for rendering logic to determine if the module
+// is a client boundary.
+const { __esModule: Mobilebar_esModule, $$typeof: Mobilebar_$$typeof } = Mobilebar_proxy;
+const Mobilebar_default_ = Mobilebar_proxy.default;
+
+
+/* harmony default export */ const Mobilebar = (Mobilebar_default_);
+;// CONCATENATED MODULE: ./src/components/Layout/Mobilebar/index.ts
+
+
+;// CONCATENATED MODULE: ./src/app/layout.tsx
+
+
+
+
+
+const metadata = {
+    title: "Ark Fi - DeFi Simplified",
+    description: "Ark Finance"
+};
+function RootLayout({ children }) {
+    return /*#__PURE__*/ jsx_runtime_.jsx("html", {
+        lang: "en",
+        className: "scroll-smooth",
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("body", {
+            className: "",
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx("main", {
+                    className: "overflow-hidden mx-auto lg:pt-[15vh] pt-[60px]",
+                    children: children
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx(Mobilebar, {}),
+                /*#__PURE__*/ jsx_runtime_.jsx(Sidebar, {})
+            ]
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 73881:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var next_dist_lib_metadata_get_metadata_route__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(80085);
+/* harmony import */ var next_dist_lib_metadata_get_metadata_route__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(next_dist_lib_metadata_get_metadata_route__WEBPACK_IMPORTED_MODULE_0__);
+  
+
+  /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((props) => {
+    const imageData = {"type":"image/x-icon","sizes":"134x135"}
+    const imageUrl = (0,next_dist_lib_metadata_get_metadata_route__WEBPACK_IMPORTED_MODULE_0__.fillMetadataSegment)(".", props.params, "favicon.ico")
+
+    return [{
+      ...imageData,
+      url: imageUrl + "",
+    }]
+  });
+
+/***/ }),
+
+/***/ 5023:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 56155:
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
