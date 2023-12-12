8e82fad878dd33a3090624b0f38871542b565c30
alexdevogel <alexdevogel@gmail.com>
2023-12-12 13:09:07 +0100

    test2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const nextConfig = {
-  output: 'standalone',
-  images: { unoptimized: true },
-  reactStrictMode: false,
-  experimental: {
-      forceSwcTransforms: true,
-  },
+    output: 'standalone',
+    images: { unoptimized: true },
+    reactStrictMode: false,
+    experimental: {
+        forceSwcTransforms: true,
+    },
+    transpilePackages: ["@0xsquid/widget"],
 };
 
 module.exports = nextConfig;
